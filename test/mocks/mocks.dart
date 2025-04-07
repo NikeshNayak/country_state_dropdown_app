150b13469677d9fcb756748207758cbbab65c134
NikeshNayak <nikeshnayak10@gmail.com>
2025-04-07 13:50:58 +0530

    test : add test of models & repository
    - add mockito for mock data in the repository
    - add test for countries_model & states_model
    - add test for location_usecase using mock repository

---
+++ test/mocks/mocks.dart
@@ -0,0 +1,5 @@
+import 'package:country_state_dropdown_app/features/location/domain/repository/location_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([LocationRepository])
+void main() {}
